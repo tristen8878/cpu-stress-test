e2ae33202e2d66661cb2a154678f36732d5a58c3
tristen8878 <tristenkearney@gmail.com>
2026-05-13 21:59:21 +0100

    Refactor worker.js to streamline work function

--- worker.js
+++ worker.js
@@ -1,20 +1,15 @@
 let running = false;
 
 function work() {
-  let count = 0;
-
   function loop() {
     if (!running) return;
 
     let ops = 0;
 
-    // heavy CPU work chunk
     for (let i = 0; i < 2e6; i++) {
       Math.sqrt(i * Math.random());
       ops++;
     }
-
-    count += ops;
 
     postMessage({
       type: "result",
